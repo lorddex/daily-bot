3fd19beeec9b6391884777bb169785da52ea5fd1
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-05-28 01:10:37 +0200

    added two endpoints

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
+from flask import request
+
 from app import app
+from app.models import Message
 
 
-@app.route('/')
-def hello_world():
+@app.route('/add')
+def hello_world_add():
+    message = Message(user='a', message='b')
+    message.save()
+    return 'Hello, world!'
+
+
+@app.route('/read')
+def hello_world_read():
+    message = Message()
+    message.save()
     return 'Hello, world!'
 
