902340e49b7370142566d7b16abcad644bebcb96
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-05-28 01:15:39 +0200

    fixed views

--- app/views.py
+++ app/views.py
@@ -1,19 +1,19 @@
 from flask import request
 
-from app import app
+from app import app, db
 from app.models import Message
 
 
 @app.route('/add')
 def hello_world_add():
     message = Message(user='a', message='b')
-    message.save()
+    db.session.add(message)
+    db.session.commit()
     return 'Hello, world!'
 
 
 @app.route('/read')
 def hello_world_read():
-    message = Message()
-    message.save()
-    return 'Hello, world!'
+    instance = db.session.query(Message).first()
+    return instance.user
 
