28cf5f8ab29a3e22136adef1f258d6813482eb33
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-05-28 16:01:29 +0200

    Updated response

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, Response
 
 from app import app, db
 from app.models import Message
@@ -7,12 +7,12 @@ from app.models import Message
 @app.route('/', methods=['POST'])
 def add_message():
     if not request.is_json:
-        return
+        return Response(status=204)
     body = request.get_json()
     message = Message(user=body['user'], message=body['message'])
     db.session.add(message)
     db.session.commit()
-    return ''
+    return Response(status=204)
 
 
 @app.route('/read')
