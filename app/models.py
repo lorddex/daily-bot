2bd00e2a0b535ea1a7b6d7371ff857785683a1b5
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-05-31 15:59:46 +0200

    added datetime

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+import datetime
 from app import db
 
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(120), index=True, unique=False)
-    message = db.Column(db.JSON(), index=False, unique=False)
+    user = db.Column(db.String(120), index=True, unique=False, nullable=False)
+    message = db.Column(db.JSON(), index=False, unique=False, nullable=False)
+    created = db.Column(db.DateTime(), index=True, nullable=False, default=datetime.datetime.now)
