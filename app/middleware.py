5a6673d7caa596fde4ca69a4b10114ceba70f00d
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-06-01 16:39:50 +0200

    Test authorisation

--- app/middleware.py
+++ app/middleware.py
@@ -27,17 +27,18 @@ class SlackSignCheckMiddleware(object):
         timestamp = request.headers.get('X-Slack-Request-Timestamp')
         sign = request.headers.get('X-Slack-Signature')
         body = request.get_data().decode("utf-8")
+
         if not timestamp or not sign or not body:
-            return
+            self.app(environ, start_response)
+
         now = datetime.datetime.now()
         #if now.timestamp() - timestamp > 60 * 5:
         #    return
-        self.app.logger.warning('SIGNATURE: ' + sign)
+
         to_sign = SLACK_SIGN_VERSION + ':' + timestamp + ':' + body
-        self.app.logger.warning('TO SIGN: ' + to_sign)
         calc_sign = hmac_sign(SLACK_SIGN_SECRET, to_sign)
-        self.app.logger.warning('SIGNED: ' + calc_sign)
         if sign != calc_sign:
             res = Response(u'Authorization failed', mimetype='text/plain', status=401)
             return res(environ, start_response)
+
         return self.app(environ, start_response)
