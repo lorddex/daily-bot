a9101ee4a0dc86bbdb1b11600e5184a3755b098b
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-06-01 15:44:33 +0200

    Test authorisation

--- app/middleware.py
+++ app/middleware.py
@@ -24,7 +24,13 @@ class SlackSignCheckMiddleware(object):
     def __call__(self, environ, start_response):
         request = Request(environ)
         sign = request.headers.get('x-slack-signature')
-        if sign != hmac_sign(SLACK_SIGN_SECRET, json.dumps(request.get_json())):
+        body = json.dumps(request.get_json())
+        self.app.logger.warning(sign)
+        self.app.logger.warning(SLACK_SIGN_SECRET)
+        self.app.logger.warning(body)
+        calc_sign = hmac_sign(SLACK_SIGN_SECRET, body)
+        self.app.logger.warning(calc_sign)
+        if sign != calc_sign:
             res = Response(u'Authorization failed', mimetype='text/plain', status=401)
             return res(environ, start_response)
         return self.app(environ, start_response)
