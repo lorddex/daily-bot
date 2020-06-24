22a4cfe130786d6bc0420560e5285deb7ea20edd
Francesco Apollonio <f.apollonio@ldlabs.org>
2020-06-25 00:08:06 +0200

    Added License

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+#  Daily-Bot
+#  Copyright (C) 2020  Francesco Apollonio
+#
+#  This file is part of Daily-Bot.
+#  Daily-Bot is free software:
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import datetime
 
 from app import db
